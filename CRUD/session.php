b267439960474a7c13a96d5dea0a23bcd081cbf1
afo-nso <83470359+afo-nso@users.noreply.github.com>
2021-07-20 17:36:30 -0300

    Create session.php

---
+++ CRUD/session.php
@@ -0,0 +1,13 @@
+<?php 
+
+session_start();
+
+// Verifica se existe os dados da sessão de login
+if(!isset($_SESSION['login']))
+{
+// Usuário não logado! Redireciona para a página de login
+header("Location: login.php");
+exit;
+}
+?>
+    
