e36410f79a847f58568b4a6682247323db8a7425
afo-nso <83470359+afo-nso@users.noreply.github.com>
2021-07-20 17:21:34 -0300

    Create login.php

---
+++ CRUD/login.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" type="text/css" href="estilo.css">
+
+<form action="config.php" method="POST" id="container">
+    <div id="login-div">
+        <div id="login-inputs">
+            Login:<input type="text" name="login" id="login"><br>
+            Senha:<input type="password" name="senha" id="senha"><br>
+        </div>
+        <input id="login-submit" type="submit" value="Login">
+    </div>
+</form>
+
+<?php 
+    
+?>
